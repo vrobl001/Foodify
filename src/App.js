341653aa448882dd8d8b0c31d606f4768f78ec3b
Vincent Robles <sylkisgsx@gmail.com>
2020-02-18 20:31:03 -0500

    added changes to layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="App-outer-container">
+      <Navbar />
+      <div className="App-inner-container">
+        <Footer />
+      </div>
     </div>
   );
 }
