327fb27baccf598b69afc5960947f64556e9e9f1
Vincent Robles <sylkisgsx@gmail.com>
2020-02-22 15:31:37 -0500

    created login form boilerplate and imported module

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Login.module.css'
+import LoginForm from '../../components/LoginForm/LoginForm'
+
+const Login = (props) => {
+    return(
+        <main className={styles.Login}>
+            <h1>Login</h1>
+            <LoginForm />
+        </main>
+    );
+}
+
+export default Login;
