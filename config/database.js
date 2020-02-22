f8fbb566b6fdaa600a70f33804e730aee458d600
Vincent Robles <sylkisgsx@gmail.com>
2020-02-22 12:09:15 -0500

    adds boilerplate for database connection

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+db.on('connected', function() {
+    console.log(`MongoDB connected on ${db.host}:${db.port}`)
+})
