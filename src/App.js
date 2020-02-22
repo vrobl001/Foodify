c970b725048d86972db5bf3d8182ebde63a357a4
Vincent Robles <sylkisgsx@gmail.com>
2020-02-22 15:35:11 -0500

    imported page modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// Reusable Components
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+
+// Page Components
+import Home from './pages/Home/Home';
+import Restaurants from './pages/Restaurants/Restaurants';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
+
 import './App.css';
 
 function App() {
@@ -8,8 +19,27 @@ function App() {
     <div className="App-outer-container">
       <Navbar />
       <div className="App-inner-container">
-        <Footer />
+        <Switch>
+
+          <Route exact path="/" render={props =>
+            <Home />
+          } />
+
+          <Route exact path="/restaurants" render={props =>
+            <Restaurants />
+          } />
+
+          <Route exact path="/login" render={props =>
+            <Login />
+          } />
+
+          <Route exact path="/signup" render={props =>
+            <Signup />
+          } />
+
+        </Switch>
       </div>
+      <Footer />
     </div>
   );
 }
