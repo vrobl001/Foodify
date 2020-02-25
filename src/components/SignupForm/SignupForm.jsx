0d88d72bd934f64a060ab76a230bf602d29cb34d
Vincent Robles <sylkisgsx@gmail.com>
2020-02-25 18:57:29 -0500

    created logic for an invalid form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,76 +1,94 @@
 import React, { Component } from 'react';
 import styles from './SignupForm.module.css';
+import userService from '../../utils/userService';
 
 class SignupForm extends Component {
-
     state = this.getInitialState();
 
     getInitialState() {
         return {
-            name: "",
-            email: "",
-            password: "",
-            passwordConf: "",
-        }
+            name: '',
+            email: '',
+            password: '',
+            passwordConf: ''
+        };
     }
+
+    isFormValid = () => {
+        return (
+            this.state.name &&
+            this.state.email &&
+            this.state.password &&
+            this.state.password === this.state.passwordConf
+        );
+    };
 
     handleChange = e => {
         this.setState({
             [e.target.name]: e.target.value
         });
-    }
+    };
 
-    handleSubmit = e => {
+    handleSubmit = async e => {
         e.preventDefault();
-
-        this.setState(this.getInitialState());
-    }
+        if (!this.isFormValid()) return;
+        try {
+            const { name, email, password } = this.state;
+            await userService.signup({ name, email, password });
+            this.setState(this.getInitialState(), () => {
+                // redirect to the /restaurants or /
+                alert('user signedup!');
+            });
+        } catch (error) {}
+    };
 
     render() {
-        return(
+        return (
             <form onSubmit={this.handleSubmit} className={styles.form}>
                 <fieldset>
                     <legend>Signup Form</legend>
-                    <label htmlFor="name">Full Name</label>
-                    <input 
-                        id="name" 
-                        name="name" 
-                        type="text" 
-                        value={this.state.name} 
+                    <label htmlFor='name'>Full Name</label>
+                    <input
+                        id='name'
+                        name='name'
+                        type='text'
+                        value={this.state.name}
                         onChange={this.handleChange}
                     />
 
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        id="email" 
-                        name="email" 
-                        type="email" 
-                        value={this.state.email} 
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        id='email'
+                        name='email'
+                        type='email'
+                        value={this.state.email}
                         onChange={this.handleChange}
                     />
 
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        id="password" 
-                        name="password" 
-                        type="password" 
-                        value={this.state.password} 
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                        value={this.state.password}
                         onChange={this.handleChange}
                     />
 
-                    <label htmlFor="passwordConf">Password Confirmation</label>
-                    <input 
-                        id="passwordConf" 
-                        name="passwordConf" 
-                        type="password" 
-                        value={this.state.passwordConf} 
+                    <label htmlFor='passwordConf'>Password Confirmation</label>
+                    <input
+                        id='passwordConf'
+                        name='passwordConf'
+                        type='password'
+                        value={this.state.passwordConf}
                         onChange={this.handleChange}
                     />
 
-                    <button type="submit">Submit</button>
+                    <button disabled={!this.isFormValid()} type='submit'>
+                        Submit
+                    </button>
                 </fieldset>
             </form>
-        )
+        );
     }
 }
 
