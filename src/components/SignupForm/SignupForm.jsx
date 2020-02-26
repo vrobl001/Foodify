77b2a100a7e025362937ed5f7f52f4caf1d621e1
Vincent Robles <sylkisgsx@gmail.com>
2020-02-25 19:29:44 -0500

    displays error message if email taken

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -10,7 +10,8 @@ class SignupForm extends Component {
             name: '',
             email: '',
             password: '',
-            passwordConf: ''
+            passwordConf: '',
+            error: ''
         };
     }
 
@@ -25,7 +26,8 @@ class SignupForm extends Component {
 
     handleChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            error:'',
+            ...{[e.target.name]: e.target.value}
         });
     };
 
@@ -36,57 +38,70 @@ class SignupForm extends Component {
             const { name, email, password } = this.state;
             await userService.signup({ name, email, password });
             this.setState(this.getInitialState(), () => {
-                this.props.history.push('/')
+                this.props.history.push('/');
             });
-        } catch (error) {}
+        } catch (error) {
+            this.setState({
+                name: '',
+                email: '',
+                password: '',
+                passwordConf: '',
+                error: error.message
+            });
+        }
     };
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit} className={styles.form}>
-                <fieldset>
-                    <legend>Signup Form</legend>
-                    <label htmlFor='name'>Full Name</label>
-                    <input
-                        id='name'
-                        name='name'
-                        type='text'
-                        value={this.state.name}
-                        onChange={this.handleChange}
-                    />
+            <section className={styles.section}>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleSubmit}>
+                    <fieldset>
+                        <legend>Signup Form</legend>
+                        <label htmlFor='name'>Full Name</label>
+                        <input
+                            id='name'
+                            name='name'
+                            type='text'
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
 
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        id='email'
-                        name='email'
-                        type='email'
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            id='email'
+                            name='email'
+                            type='email'
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
 
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        id='password'
-                        name='password'
-                        type='password'
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                    />
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            id='password'
+                            name='password'
+                            type='password'
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
 
-                    <label htmlFor='passwordConf'>Password Confirmation</label>
-                    <input
-                        id='passwordConf'
-                        name='passwordConf'
-                        type='password'
-                        value={this.state.passwordConf}
-                        onChange={this.handleChange}
-                    />
+                        <label htmlFor='passwordConf'>
+                            Password Confirmation
+                        </label>
+                        <input
+                            id='passwordConf'
+                            name='passwordConf'
+                            type='password'
+                            value={this.state.passwordConf}
+                            onChange={this.handleChange}
+                        />
 
-                    <button disabled={!this.isFormValid()} type='submit'>
-                        Submit
-                    </button>
-                </fieldset>
-            </form>
+                        <button disabled={!this.isFormValid()} type='submit'>
+                            Submit
+                        </button>
+                    </fieldset>
+                </form>
+            </section>
         );
     }
 }
