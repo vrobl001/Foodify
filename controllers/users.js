6485e1b712555be413286497cecbd780d07bd51d
Vincent Robles <sylkisgsx@gmail.com>
2020-02-25 20:07:28 -0500

    created user token

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
-const User = require("../models/user");
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
 module.exports = {
     signup
@@ -7,8 +9,14 @@ module.exports = {
 async function signup(req, res) {
     try {
         const user = await User.create(req.body);
-        res.json(user);
+        const token = createJWT(user);
+        res.json({ token });
     } catch (error) {
+        console.log(error)
         res.status(400).json(error);
     }
+}
+
+function createJWT(user) {
+    return jwt.sign({ user }, SECRET, { expiresIn: '24h' });
 }
