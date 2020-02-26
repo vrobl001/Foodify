0aea6115c223fa615a4f77ea1fb13e4b76c369f1
Vincent Robles <sylkisgsx@gmail.com>
2020-02-25 20:07:45 -0500

    removes sensitive info before making json

--- models/user.js
+++ models/user.js
@@ -1,31 +1,40 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 6;
 
 const userSchema = new Schema(
-  {
-    name: String,
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true
+    {
+        name: String,
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: String
     },
-    password: String
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
-userSchema.pre("save", function(next) {
-  const user = this;
-  if (!user.isModified("password")) return next();
-  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
-    if (err) return next(err);
-    user.password = hash;
-    next();
-  });
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+        delete ret.password;
+        delete ret.createdAt;
+        delete ret.updatedAt;
+        delete ret.__v;
+    }
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
