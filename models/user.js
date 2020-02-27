c7c2558ddb95b6125503fca09d9b9be36254ff17
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 20:35:48 -0500

    created compare password method

--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
+userSchema.methods.comparePassword = function(tryPassword, callback) {
+    bcrypt.compare(tryPassword, this.password, callback);
+};
+
 userSchema.set('toJSON', {
     transform: function(doc, ret) {
         delete ret.password;
