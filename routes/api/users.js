7d52341ecce8e1f8b5abcbe0ae232f124f3e95af
Vincent Robles <sylkisgsx@gmail.com>
2020-02-26 20:36:03 -0500

    made route for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const usersCtrl = require('../../controllers/users');
 
 router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
 
 module.exports = router;
